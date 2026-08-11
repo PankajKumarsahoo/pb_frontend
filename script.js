7ca6045dffdda82b06ea133e2a086dad833314c8
PankajKumarsahoo <pankajsahoo8260@gmail.com>
2026-08-11 16:10:21 +0530

    changes on live api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // FastAPI backend URL
-const API_URL = "http://16.192.61.123:8000/chat";
+const API_URL = "https://16.192.61.123:8000/chat";
 
 const STORAGE_KEY = "pratibhabatee_chat_history";
  
